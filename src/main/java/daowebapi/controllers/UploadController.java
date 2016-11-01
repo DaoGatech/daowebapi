2acd41f36bd0c403d0541cf827616ad691e7b4a2
DaoGatech <stephenhdao@gatech.edu>
2016-11-01 14:55:37 -0400

    add uploadcontroller

---
+++ src/main/java/daowebapi/controllers/UploadController.java
@@ -0,0 +1,37 @@
+package daowebapi.controllers;
+
+/**
+ * Created by tmizzle2005 on 11/1/16.
+ */
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadController {
+
+    @RequestMapping(value="/upload", method= RequestMethod.POST)
+    public @ResponseBody
+    String handleFileUpload(
+            @RequestParam("file") MultipartFile file){
+        String name = "test11";
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
+
+}
--- src/main/java/daowebapi/controllers/UserController.java
+++ src/main/java/daowebapi/controllers/UserController.java
@@ -4,9 +4,7 @@ package daowebapi.controllers;
  * Created by tmizzle2005 on 10/31/16.
  */
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import daowebapi.models.User;
