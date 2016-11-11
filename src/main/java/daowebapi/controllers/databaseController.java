29e326a72a955ee545e478e045df7e9b9dd644ad
DaoGatech <stephenhdao@gatech.edu>
2016-11-11 00:15:35 -0500

    add database controller

---
+++ src/main/java/daowebapi/controllers/databaseController.java
@@ -0,0 +1,23 @@
+package daowebapi.controllers;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by tmizzle2005 on 11/10/16.
+ */
+public class databaseController {
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+}
