5cd6fdab41f0621d2f5672ec1bdace0d5df7d946
DaoGatech <stephenhdao@gatech.edu>
2016-12-22 22:42:39 -0700

    remove unused imports

--- src/main/java/daowebapi/controllers/Album/UserController.java
+++ src/main/java/daowebapi/controllers/Album/UserController.java
@@ -1,20 +1,11 @@
 package daowebapi.controllers.Album;
 
-/**
- * Created by tmizzle2005 on 10/31/16.
- */
-
 import daowebapi.models.Image;
 import org.springframework.web.bind.annotation.*;
 import daowebapi.models.User;
 import java.util.ArrayList;
 import java.util.HashMap;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.AWSCredentials;
+
 
 @RestController
 public class UserController {
